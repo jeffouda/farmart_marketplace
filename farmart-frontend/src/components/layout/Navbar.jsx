aa3d5dea40d9db9a24de5deb8e54efa3ba994ebd
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-02-02 15:47:18 +0300

    Change logo icon to cow

--- farmart-frontend/src/components/layout/Navbar.jsx
+++ farmart-frontend/src/components/layout/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
-            <span className="text-2xl">🌾</span>
+            <span className="text-2xl">🐄</span>
             <span className="text-xl font-bold text-green-600">FarmAT</span>
           </Link>
 
