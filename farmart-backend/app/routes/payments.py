71aa8fc22664a65c2ee4b4b098afbd268cea30cf
Enock Cheruiyot <cheruiyote518@gmail.com>
2026-02-05 10:10:51 +0300

    passes a function to trigger stk push

--- farmart-backend/app/routes/payments.py
+++ farmart-backend/app/routes/payments.py
@@ -2,3 +2,9 @@ from flask import Blueprint, request, jsonify
 from app.services.mpesa_service import send_stk_push
 
 payments_bp = Blueprint('payments', __name__)
+
+@payments_bp.route('/stk-push', methods=['POST'])
+def trigger_stk():
+    data = request.get_json()
+    phone = data.get('phoneNumber')
+    amount = data.get('amount', 1)
