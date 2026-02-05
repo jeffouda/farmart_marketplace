d58baa640e5d0ef0a18e568d7bae1a363f30c001
Enock Cheruiyot <cheruiyote518@gmail.com>
2026-02-05 09:58:08 +0300

    implements access token function

--- farmart-backend/app/services/mpesa_service.py
+++ farmart-backend/app/services/mpesa_service.py
@@ -2,3 +2,11 @@ import requests
 import base64
 from datetime import datetime
 from flask import current_app
+
+# Access token
+
+def get_access_token():
+    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    consumer_key = current_app.config['MPESA_CONSUMER_KEY']
+    consumer_secret = current_app.config['MPESA_CONSUMER_SECRET']
+    
